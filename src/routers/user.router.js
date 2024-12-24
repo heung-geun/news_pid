c949f62ede808136ec018cb913be8422d14f44a4
lgm-7 <xlzlrud@naver.com>
2024-12-24 16:25:57 +0900

    add user.router
    프로필수정

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { prisma } from "../utils/prisma/index.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -25,4 +26,27 @@ router.post("/users", async (req, res, next) => {
   return res.status(201).json({ message: "회원가입이 완료되었습니다." });
 });
 
+router.patch(
+  "/me/:userId",
+  /*authMiddleware,*/ async (req, res, next) => {
+    const { userId } = req.params;
+    const { nickname, interest, introduce } = req.body;
+    const userInfo = await prisma.user.findFirst({
+      where: { userId: +userId },
+    });
+    console.log(userInfo);
+    const updateProfile = await prisma.user.update({
+      where: {
+        userId: +userId,
+      },
+      data: {
+        nickname: nickname !== undefined ? nickname : userInfo.nickname,
+        interest: interest !== undefined ? interest : userInfo.interest,
+        introduce: introduce !== undefined ? introduce : userInfo.introduce,
+      },
+    });
+    console.log(updateProfile);
+    return res.status(200).json({ message: "프로필 수정 완료" });
+  },
+);
 export default router;
