47a43b48bedfb35775b74d4963ef6a9c9d33c18f
ohhyeseong <ohs020105@gmail.com>
2024-12-24 15:05:35 +0900

    [add] env데이터베이스 및 회원가입시 비밀번호 해쉬하는거 추가했습니다.

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 import { prisma } from "../utils/prisma/index.js";
 
 const router = express.Router();
@@ -7,7 +8,7 @@ const router = express.Router();
 router.post("/users", async (req, res, next) => {
   const { email, password, nickname, name, age, interest, introduce } =
     req.body;
-  const isExistUser = await prisma.User.findFirst({
+  const isExistUser = await prisma.User.findUnique({
     where: {
       email,
     },
@@ -16,10 +17,20 @@ router.post("/users", async (req, res, next) => {
   if (isExistUser) {
     return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
   }
-
+  // 사용자 비밀번호를 암호화합니다.
+  const hashedPassword = await bcrypt.hash(password, 10);
   // Users 테이블에 사용자를 추가합니다.
-  const user = await prisma.User.create({
-    data: { email, password, nickname, name, age, interest, introduce },
+
+  const User = await prisma.User.create({
+    data: {
+      email,
+      password: hashedPassword,
+      nickname,
+      name,
+      age,
+      interest,
+      introduce,
+    },
   });
 
   return res.status(201).json({ message: "회원가입이 완료되었습니다." });
