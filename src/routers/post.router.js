3a5827066d68ca920456efa3cef86cd8608c02f3
Hyeunggeun-Yun <gudrms121712@naver.com>
2024-12-24 21:06:54 +0900

    add 게시글 전체 조회, 게시글 장르lol 조회
    -게시글 전체 조회 구현
    -게시글 장르 lol 조회 구현

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -4,6 +4,26 @@ import { prisma } from "../utils/prisma/index.js";
 const router = express.Router();
 
 
-router.get("/posts/all" (req, res) => {
+router.get('/posts/all', (req, res) => {
+  const posts = prisma.posts.findMany({
+    select: {
+      userId: true,
+      title: true,
+      createdAt: true,
+    },
+    orderBy: {
+      createdAt: 'desc', // 게시글을 최신순으로 정렬
+    },
+  });
 
-})
+  return res.status(200).json({ data: posts });
+});
+
+
+router.get('/posts/lol', (req, res) => {
+  const postsLol = prisma.posts.filter(type => {
+    type.type === lol
+  });
+
+  return res.status(200).json({ data: postsLol });
+});
