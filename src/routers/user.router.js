50178d80dd6268ce44854445efb2e2177d042f81
lgm-7 <xlzlrud@naver.com>
2024-12-26 11:35:09 +0900

    fix 프로필인증관련

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -142,60 +142,54 @@ router.post("/auth", async (req, res) => {
 /** 사용자 로그아웃 기능 */
 
 /*사용자 프로필 조회*/
-router.get(
-  "/me/:userId",
-  /*authMiddleware,*/ async (req, res, next) => {
-    const { userId } = req.params;
-    const userInfo = await prisma.user.findFirst({
-      where: { userId: +userId },
-      select: {
-        name: true,
-        nickname: true,
-        interest: true,
-        introduce: true,
-        age: true,
-      },
-    });
-    return res.status(200).json({ data: userInfo });
-  },
-);
+router.get("/me/:userId", authMiddleware, async (req, res, next) => {
+  const { userId } = req.user;
+  const userInfo = await prisma.user.findFirst({
+    where: { userId: +userId },
+    select: {
+      name: true,
+      nickname: true,
+      interest: true,
+      introduce: true,
+      age: true,
+    },
+  });
+  return res.status(200).json({ data: userInfo });
+});
 
 /*사용자 프로필 수정*/
-router.patch(
-  "/me/:userId",
-  /*authMiddleware,*/ async (req, res, next) => {
-    const { userId } = req.params;
-    const { password, passwordCheck, nickname, interest, introduce, age } =
-      req.body;
-    const userInfo = await prisma.user.findFirst({
-      where: { userId: +userId },
-    });
-    if (password && !passwordCheck) {
-      return res.status(404).json({ message: "비밀번호 확인을 입력해주세요" });
-    }
-    if (passwordCheck !== password) {
-      return res
-        .status(401)
-        .json({ message: "변경할 비밀번호가 동일하지 않습니다" });
-    }
+router.patch("/me/:userId", authMiddleware, async (req, res, next) => {
+  const { userId } = req.user;
+  const { password, passwordCheck, nickname, interest, introduce, age } =
+    req.body;
+  const userInfo = await prisma.user.findFirst({
+    where: { userId: +userId },
+  });
+  if (password && !passwordCheck) {
+    return res.status(404).json({ message: "비밀번호 확인을 입력해주세요" });
+  }
+  if (passwordCheck !== password) {
+    return res
+      .status(401)
+      .json({ message: "변경할 비밀번호가 동일하지 않습니다" });
+  }
 
-    const updateData = {
-      email: email !== undefined ? email : userInfo.email,
-      nickname: nickname !== undefined ? nickname : userInfo.nickname,
-      interest: interest !== undefined ? interest : userInfo.interest,
-      introduce: introduce !== undefined ? introduce : userInfo.introduce,
-      age: age !== undefined ? age : userInfo.age,
-    };
+  const updateData = {
+    email: email !== undefined ? email : userInfo.email,
+    nickname: nickname !== undefined ? nickname : userInfo.nickname,
+    interest: interest !== undefined ? interest : userInfo.interest,
+    introduce: introduce !== undefined ? introduce : userInfo.introduce,
+    age: age !== undefined ? age : userInfo.age,
+  };
 
-    if (password) {
-      updateData.password = await bcrypt.hash(password, 10);
-    }
+  if (password) {
+    updateData.password = await bcrypt.hash(password, 10);
+  }
 
-    await prisma.user.update({
-      where: { userId: +userId },
-      data: updateData,
-    });
-    return res.status(200).json({ message: "프로필 수정 완료" });
-  },
-);
+  await prisma.user.update({
+    where: { userId: +userId },
+    data: updateData,
+  });
+  return res.status(200).json({ message: "프로필 수정 완료" });
+});
 export default router;
