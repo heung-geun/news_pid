d8fdfa17f254c23b281bfe02ddd01c41701841a8
thiswizard <dkaand3318@gmail.com>
2024-12-31 11:45:28 +0900

    a
    a

--- src/app.js
+++ src/app.js
@@ -5,22 +5,38 @@ import likeRouter from "./routers/like.router.js";
 import postsRouter from "./routers/post.router.js";
 import crudRouter from "./routers/crud.router.js";
 import cmRouter from "./routers/cm.router.js";
+import dotenv from "dotenv"
+import fs from "fs"
+import https from "https"
 
 const app = express();
-const PORT = 3030;
+const PORT = 3002;
 
 app.use(
   session({
     secret: process.env.SESSEION_KEY, // 비밀 키
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }, // HTTPS를 사용할 경우 true로 설정
+    cookie: { secure: true }, // HTTPS를 사용할 경우 true로 설정
   }),
 );
 app.use(express.json());
 
-app.use("/api", [UsersRouter, likeRouter, postsRouter, crudRouter, cmRouter]);
+const keypath = process.env.KEY_PATH
+const certpath = process.env.CERT_PATH
 
-app.listen(PORT, () => {
-  console.log(PORT, "3030포트로 서버가 열렸어요!");
-});
+const key = fs.readFileSync(keypath)
+const cert = fs.readFileSync(certpath)
+
+
+const option = {key: key , cert: cert}
+
+
+app.use("/api", [UsersRouter, likeRouter, postsRouter, crudRouter, cmRouter]);
+app.use("/hello", (req ,res) => {
+  res.send({message: "hello world"})
+})
+
+https.createServer(option, app).listen(PORT, () => {
+  console.log("hello world")
+})
